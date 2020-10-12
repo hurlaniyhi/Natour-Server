917cea72c5eeca7845b264c8621dc2ac8637723f
Ridwan Kolawole <olaniyi.jibola152@gmail.com>
2020-10-12 21:27:37 +0400

    make it better

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require('express')
 const path = require('path')
-var port = process.env.PORT || 5000
+var port = process.env.PORT||3050
 
 const app = express()
 
 app.use(express.static(path.join(__dirname, 'display')))
 
 app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname,'display','index.html'))
+    // res.sendFile(path.join(__dirname,'display','index.html'))
+    res.send("okay")
 })
 
-app.listen(3050, ()=> {
-    console.log(`server is running on port ${port}`)
+app.listen(port, ()=> {
+    console.log(`server is running on port ${port}`)  
 })
